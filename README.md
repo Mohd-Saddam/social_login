f769a5378a3fb434f24aa5d008172080a149e276
Mohd saddam husain <msaddam2786@gmail.com>
2020-07-06 16:44:47 +0530

    test_social_login

---
+++ README.md
@@ -0,0 +1,17 @@
+To run this project:
+Backend:
+
+
+cd sadam-bhai-auth-test
+
+install virtualenv, then activate virtualenv -
+
+pip3 install virtualenv
+virutalenv venv
+source venv/bin/activate
+install requirements.txt -
+
+pip3 install -r requirements.txt
+cd mypro
+
+python3 manage.py runserver 0.0.0.0:8000
---
+++ mypro/authapp/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from authapp import views as core_views
+
+
+urlpatterns = [
+    path('', core_views.home, name='home'),
+    url( r'^accounts/login/$',auth_views.LoginView.as_view(template_name="authapp/login.html"), name="login"),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        core_views.activate, name='activate'),
+
+]
